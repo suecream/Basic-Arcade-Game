eda5216a8fe9cc7e46160a5b7237745ad4622f66
Hojun Kim <superlove133@gmail.com>
2022-02-24 00:13:27 +0900

    main sprite

--- pygame_basic/1_create_frame.py
+++ pygame_basic/1_create_frame.py
@@ -1,3 +1,4 @@
+from turtle import Screen
 import pygame
 
 pygame.init()
@@ -13,6 +14,15 @@ pygame.display.set_caption("NaDo GaMe")
 # background set
 background = pygame.image.load("C:/Users/HJ/Documents/PythonWorkSpace/pygame_basic/background.png")
 
+# 스프라이트(캐릭터) 불러오기
+character = pygame.image.load("C:/Users/HJ/Documents/PythonWorkSpace/pygame_basic/character.png")
+character_size = character.get_rect().size # 이미지 크기를 구해옴
+character_width = character_size[0] # 가로크기
+character_height = character_size[1] # 세로크기
+character_x_pos = (screen_width / 2) - character_width/2 # 화면 가로의 절반에 위치
+character_y_pos = screen_height -character_height# 화면 세로의 가장 아래 위치
+
+
 
 # event loop
 running = True
@@ -23,8 +33,12 @@ while running:
     screen.blit(background, (0,0)) # 배경 그리기
     # screen.fill((0,0,255)) fill로 채워도 된다
 
+    screen.blit(character, (character_x_pos,character_y_pos)) # 캐릭터 그리기
+
+
     pygame.display.update() # 배경 다시 그리기(반복)
 pygame.quit()
+
 
 
 
