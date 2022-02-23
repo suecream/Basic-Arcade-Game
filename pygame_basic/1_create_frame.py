abb3ad3c3925ee3602ce03ee8d8d4a137a495041
Hojun Kim <superlove133@gmail.com>
2022-02-23 23:32:24 +0900

    frame set

--- pygame_basic/1_create_frame.py
+++ pygame_basic/1_create_frame.py
@@ -1,1 +1,28 @@
 import pygame
+
+pygame.init()
+
+# screen size
+screen_width = 800 # horizontal
+screen_height = 640 # vertical
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# title
+pygame.display.set_caption("NaDo GaMe")
+
+# event loop
+running = True
+while running:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트
+            running = False
+
+
+pygame.quit()
+
+
+
+
+
+
+
