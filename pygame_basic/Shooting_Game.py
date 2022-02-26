a9086cce57f24d865db41bf80cebc6869a8b14a8
Hojun Kim <superlove133@gmail.com>
2022-02-27 02:17:00 +0900

    2_game frame set

---
+++ pygame_basic/Shooting_Game.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+
+pygame.init()
+
+screen_width = 800
+screen_height = 640
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+pygame.display.set_caption("Shooting_Game")
+background = pygame.image.load("pygame_basic/background.png")
+
