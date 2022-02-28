3c498c9db02f9075d671a173fedab8a620bae0f2
Hojun Kim <superlove133@gmail.com>
2022-03-01 01:46:56 +0900

    ball bounce set

--- pygame_project1/Arcade_Pang_Game.py
+++ pygame_project1/Arcade_Pang_Game.py
@@ -60,7 +60,7 @@ balls = []
 balls.append({
     "pos_x" : 50,
     "pos_y" : 50, # ball 시작 위치
-    "image_idx" : 0, # ball index
+    "img_idx" : 0, # ball index
     "to_x" : 3, 
     "to_y" : -6, # 시작 시 이동방향
     "init_spe_y" : ball_speed_y[0] # y 최초 속도
@@ -102,11 +102,38 @@ while running:
     # 천장에 닿은 weapon 처리
     weapons = [[w[0],w[1]] for w in weapons if w[1] > 0]
 
+    # ball 위치 정의
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        ball_size = ball_images[ball_img_idx].get_rect().size
+        ball_width = ball_size[0]
+        ball_height = ball_size[1]
+        # 벽에 닿은 경우 튕기기
+        if ball_pos_x <= 0 or ball_pos_x > screen_width - ball_width:
+               ball_val["to_x"] = ball_val["to_x"] * -1
+
+        if ball_pos_y >= screen_height - stage_height - ball_height:
+            ball_val["to_y"] = ball_val["init_spe_y"]
+        else:
+            ball_val["to_y"] = ball_val["to_y"] + 0.5 
+
+        ball_val["pos_x"] += ball_val["to_x"]
+        ball_val["pos_y"] += ball_val["to_y"]
+
+
+
     screen.blit(background, (0,0))
 
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon,(weapon_x_pos,weapon_y_pos))
-
+    for idx, val in enumerate(balls):
+        ball_pos_x = val["pos_x"]
+        ball_pos_y = val["pos_y"]
+        ball_img_idx = val["img_idx"]
+        screen.blit(ball_images[ball_img_idx],(ball_pos_x, ball_pos_y))
     screen.blit(stage,(0,screen_height-stage_height))
     screen.blit(character,(character_x_pos,character_y_pos))
     
