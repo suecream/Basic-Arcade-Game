89aff97cf1665ff20db75eb32c693605578be901
Hojun Kim <superlove133@gmail.com>
2022-03-01 20:22:38 +0900

    first colliderect set

--- pygame_project1/Arcade_Pang_Game.py
+++ pygame_project1/Arcade_Pang_Game.py
@@ -67,6 +67,11 @@ balls.append({
 
 })
 
+# 사라질 무기, 공 정보 저장 변수
+weapon_to_remove = -1
+ball_to_remove = -1
+
+
 running = True
 while running:
     dt = clock.tick(60)
@@ -123,6 +128,49 @@ while running:
         ball_val["pos_x"] += ball_val["to_x"]
         ball_val["pos_y"] += ball_val["to_y"]
 
+
+    # 캐릭터 rect 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        # ball rect 업데이트
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+        # ball & character 충돌처리 
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+        # ball & weapon 충돌처리
+        for weapon_idx, weapon_val in enumerate(weapons):
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+
+            # weapon rect 업데이트
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+
+            # 충돌 체크
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx # 해당 무기 없애기 위한 값
+                ball_to_remove = ball_idx
+                break
+
+    # 충돌된 공 or 무기 없애기
+    if ball_to_remove > -1:
+        del balls[ball_to_remove]
+        ball_to_remove = -1
+    if weapon_to_remove > -1:
+        del weapons[weapon_to_remove]
+        weapon_to_remove = -1
+        
 
 
     screen.blit(background, (0,0))
